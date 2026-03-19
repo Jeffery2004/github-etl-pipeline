d949f3a0e4fdfc4e5ead589566a152f871048c94
Jeffery Abijah <jefferyabijah@gmail.com>
2026-03-19 16:46:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import mysql.connector
+import requests
+from datetime import datetime
+import pandas as pd
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'jeffery2004',
+    'database': 'github_analytics'
+}
+
+def convert_date(date_str):
+    if date_str is None:
+        return None
+    return datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
+
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+url = "https://api.github.com/users/Jeffery2004/repos"
+response = requests.get(url)
+
+if response.status_code != 200:
+    print("Error:", response.status_code)
+    print(response.text)
+    exit()
+
+repos = response.json()
+
+insert_query = """
+INSERT IGNORE INTO github_repos 
+(repo_id, username, repo_name, language, stars, forks, created_at, pushed_at, repo_url)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+for repo in repos:
+    repo_data = (
+        repo["id"],
+        repo["owner"]["login"],
+        repo["name"],
+        repo["language"],
+        repo["stargazers_count"],
+        repo["forks_count"],
+        convert_date(repo["created_at"]),
+        convert_date(repo["pushed_at"]),
+        repo["html_url"]
+    )
+
+    cursor.execute(insert_query, repo_data)
+
+conn.commit()
+print("All repos inserted successfully:", len(repos))
+
+cursor.close()
+conn.close()
+
+df=pd.DataFrame(repos)
+df.to_csv('github_repos.csv', index=False)
+print("loaded data from csv")
